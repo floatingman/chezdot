fe3a6f530bd6f5ff649c566830a66d5a0d3c84f1
floatingman <dan@danlovesprogramming.com>
2025-02-25 19:18:42 -0600

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -6,7 +6,9 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 config.font = wezterm.font("JetBrains Mono")
-config.color_scheme = "Catppuccin Mocha"
-
+-- config.color_scheme = "Catppuccin Latte"
+config.color_scheme = "Catppuccin Macchiato"
+-- config.color_scheme = "Solarized (dark) (terminal.sexy)"
+-- config.color_scheme = "Solarized (light) (terminal.sexy)"
 -- and finally, return the configuration to wezterm
 return config
