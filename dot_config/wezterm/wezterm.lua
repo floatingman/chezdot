50d4d4a459f3944f727f6f7b6c537b536727cdf3
floatingman <dan@danlovesprogramming.com>
2025-02-25 19:40:45 -0600

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,14 +1,27 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
-
+local time = wezterm.time.now()
+local sun_times = time:sun_times(36.16, -86.78)
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 config.font = wezterm.font("JetBrains Mono")
 -- config.color_scheme = "Catppuccin Latte"
-config.color_scheme = "Catppuccin Macchiato"
+-- config.color_scheme = "Catppuccin Macchiato"
 -- config.color_scheme = "Solarized (dark) (terminal.sexy)"
+-- config.color_scheme = "AdventureTime"
 -- config.color_scheme = "Solarized (light) (terminal.sexy)"
 -- and finally, return the configuration to wezterm
+
+if sun_times.up then
+	-- Apply light theme
+	config.color_scheme = "Catppuccin Latte"
+	-- config.color_scheme = "AdventureTime"
+else
+	-- Apply dark theme
+	config.color_scheme = "Catppuccin Macchiato"
+	-- config.color_scheme = "Solarized Dark"
+end
+
 return config
